43b16fd3cf256120c7cb168d497bed4d8a563432
IDDna <romanov1371@gmail.com>
2022-10-04 21:00:14 +0300

    Create Overview.js

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 
 class Overview extends Component {
-  render() {}
+  render() {
+    return this.props.tasks.map((task, index) => {
+      return <li key={index}>{task}</li>
+    })
+  }
 }
 
 export default Overview
