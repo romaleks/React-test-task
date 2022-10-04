d294e19b9ec876ee228ca76561abdf25eca53648
IDDna <romanov1371@gmail.com>
2022-10-04 20:59:59 +0300

    Create App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react'
+import Overview from './components/Overview'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      taskTitle: '',
+      tasksArray: [],
+    }
+
+    this.handleTaskChange = this.handleTaskChange.bind(this)
+    this.addTask = this.addTask.bind(this)
+  }
+
+  handleTaskChange(ev) {
+    this.setState({
+      taskTitle: ev.target.value,
+    })
+  }
+
+  addTask() {
+    const task = this.state.taskTitle
+    if (!task) return
+    this.setState({
+      tasksArray: [...this.state.tasksArray, task],
+    })
+  }
+
   render() {
-    return <div className='App'>hello</div>
+    return (
+      <div className='App'>
+        <input
+          type='text'
+          placeholder='Task Title'
+          value={this.state.taskTitle}
+          onChange={this.handleTaskChange}
+        />
+        <button onClick={this.addTask}>Submit</button>
+        <Overview tasks={this.state.tasksArray} />
+      </div>
+    )
   }
 }
 
